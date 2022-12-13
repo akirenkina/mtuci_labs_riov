693cbd0226c00e6fce6c75d2b5e0fccea332d7d9
Kirenkina Alya <66954767+akirenkina@users.noreply.github.com>
2022-12-13 23:06:49 +0300

    Update Main.java

--- lab2/lab2.1/src/Main.java
+++ lab2/lab2.1/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] msi) {
-        Student student = new Student("Элеонора", "Булышева", "БУТ1901", 4.5);
-        Aspirant aspirant = new Aspirant("Никита", "Морозов", "БУТ1801", 4, "дрон");
-        Student stud = new Aspirant("Антон", "Бутузов", "БУТ1901", 5, "система");
+        Student student = new Student("Аля", "Киренкина", "БУТ1901", 4.6);
+        Aspirant aspirant = new Aspirant("Макс", "Морозов", "БУТ1851", 4, "дрон");
+        Student stud = new Aspirant("Антон", "Назаров", "БАП1901", 5, "система");
         Student[] students = { student, aspirant, stud }; //создаём массив из объектов раннее созданных классов
 
         for (int i = 0; i < students.length; ++i) {
